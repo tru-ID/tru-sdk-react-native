ab3fc09851e3f4b25b2a93a1cad7b8fa9b22f632
Didem Yakici <didematilla@gmail.com>
2024-04-23 18:42:59 +0100

    Update to v1.0.13
    Update tru-sdk-ios to v1.0.10 (Added PrivacyInfo file)

--- example/ios/Pods/Target Support Files/Pods-TruSdkReactNativeExample/Pods-TruSdkReactNativeExample-resources.sh
+++ example/ios/Pods/Target Support Files/Pods-TruSdkReactNativeExample/Pods-TruSdkReactNativeExample-resources.sh
@@ -98,9 +98,11 @@ EOM
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/React-Core/AccessibilityResources.bundle"
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/tru-sdk-ios/tru-sdk-ios.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/React-Core/AccessibilityResources.bundle"
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/tru-sdk-ios/tru-sdk-ios.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
